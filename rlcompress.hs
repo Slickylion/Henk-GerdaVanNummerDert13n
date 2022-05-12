af8fb088c098ee6d5e925b3d8cb75a8ae608c09f
MahjongBeijing <marjonbuijing@icloud.com>
2022-05-12 16:22:43 +0200

    Deel1

--- rlcompress.hs
+++ rlcompress.hs
@@ -2,9 +2,19 @@
 -- Ben Derksen (1014934)
 -- Marjon Buijing (1015470)
 module Main where
-import Data.List
-import Data.Char
+import System.IO  
+import System.Directory
 import System.Environment
+import Data.List
+
+runLength :: String -> String
+runLength = concat . map (compress) . Data.List.group 
+
+compress :: String -> String
+compress x 
+    | leng > 1 =(show . length $ x) ++ [head x]
+    | otherwise = [head x]
+    where leng = length $ x
 
 main :: IO ()
 main = do
@@ -12,9 +22,14 @@ main = do
     if length args < 2
         then putStrLn "please give file to read and file to write to"
         else do
-            let command1 = head args
-            let command2 = args !! 1
-            text <- readFile command1
-            let processed = sort text
-            writeFile command2 processed
-            putStrLn processed
+         let command1 = head args
+             command2 = args!! 1
+         text <- readFile command1;
+         let compressedText = runLength text
+         let textLength = length text
+         putStrLn("length of " ++ command1 ++ ": " ++ (show textLength) ++ " characters.")
+         let compressedTextLength = length compressedText
+         putStrLn("length of compressed file " ++ command2 ++ ": " ++ (show compressedTextLength) ++ " characters.")
+         let compressionFactor = round $ ((fromIntegral compressedTextLength) / (fromIntegral textLength)) * 100
+         putStrLn("factor: " ++ (show compressedTextLength) ++ " / " ++ (show textLength) ++ " * 100% = " ++ (show compressionFactor) ++ "%")
+         writeFile command2 compressedText

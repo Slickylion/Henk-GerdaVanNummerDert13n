a52783e437786558ccb09e06b1a2062e237f89c8
Slickylion <Slickylion@gmail.com>
2022-05-16 18:35:07 +0200

    Decompress working

--- rldecompress.hs
+++ rldecompress.hs
@@ -1,37 +1,16 @@
 import Data.Char
 import System.Environment
+import Data.List
 
-splitInput :: String -> [String]
-splitInput [] = []
-splitInput (x:y:rest) = if isDigit y
-                         then let num1 = charToString x
-                                  num2 = charToString y
-                                  tot = num1 ++ num2
-                                  c = head rest
-                                  newRest = drop 1 rest
-                               in [addToback tot c]++(splitInput newRest)
-                         else [x, y]:(splitInput rest)
-splitInput rest = [rest]--Prevent uneven list exception
 
-charToString :: Char -> String
-charToString c = [c]
+splitRunLength :: String -> [String]
+splitRunLength [] = []
+splitRunLength (x:rest)
+    | isDigit x = replicate (digitToInt x) (head rest) : end
+    | otherwise = [x] : splitRunLength rest
+        where end = splitRunLength (tail rest)
 
-addToback :: String -> Char -> String
-addToback str c = str ++ [c]
-
-decompress :: String -> String
-decompress x = 
-     let n = digitToInt $ head x
-         c = last x
-     in myReplicate n c
-
-myReplicate :: Int -> a -> [a]
-myReplicate 0 y = []
-myReplicate x y = y : myReplicate (x-1) y
-
-runLength :: String -> String
-runLength x = concat . map (decompress) . splitInput $ x
-
+main :: IO ()
 main = do
 -- Get the command line arguments and split them into two
          args <- getArgs
@@ -39,5 +18,5 @@ main = do
              outputFile = args!!1
 -- Open input file and compress inputText into outputText
          inputText <- readFile inputFile;
-         let outputText = runLength inputText
-         writeFile outputFile outputText
+         let outputText = splitRunLength inputText
+         writeFile outputFile $ concat outputText

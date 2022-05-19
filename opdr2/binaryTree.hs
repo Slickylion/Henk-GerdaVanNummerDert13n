c6ba5f92f54370b7a02571c3402834068b85049c
Slickylion <Slickylion@gmail.com>
2022-05-19 16:21:42 +0200

    Added BinTree data type

--- opdr2/binaryTree.hs
+++ opdr2/binaryTree.hs
@@ -1,3 +1,20 @@
 -- Gemaakt door:
 -- Ben Derksen (1014934)
 -- Marjon Buijing (1015470)
+
+-- pre order traversal:
+-- in order traversal:
+-- post order traversal:
+
+data BinTree a = Empty
+    | Node a (BinTree a)(BinTree a)
+     deriving Show
+
+
+
+
+
+
+
+
+

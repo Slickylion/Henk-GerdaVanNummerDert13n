d568fd9ce5a530ef0be21e0292dc984aff2d0168
Slickylion <Slickylion@gmail.com>
2022-05-23 14:02:20 +0200

    Added BinTree push function

--- opdr2/binaryTree.hs
+++ opdr2/binaryTree.hs
@@ -5,10 +5,21 @@
 -- pre order traversal:
 -- in order traversal:
 -- post order traversal:
+module Main where
+
+
+main = do
+    let x = Node 0 Empty Empty;
+    putStrLn $ show x
 
 data BinTree a = Empty
     | Node a (BinTree a)(BinTree a)
-     deriving Show
+     deriving (Show, Read)
+
+
+push :: a -> BinTree a -> BinTree a
+push x Empty = Node x Empty Empty
+push x (Node y a b) = Node y (Node x Empty Empty) b
 
 
 

844e441dd9fe56fd29c27a65bd5cecf01d03e87f
Slickylion <Slickylion@gmail.com>
2022-05-30 14:01:01 +0200

    Fixed push

--- opdr2/binaryTree.hs
+++ opdr2/binaryTree.hs
@@ -7,22 +7,22 @@
 -- post order traversal:
 module Main where
 
-
 main :: IO ()
 main = do
     let x = Node Empty 0 Empty
     putStr "binary Tree"
-    
 
 data BinTree a = Empty
     | Node (BinTree a) a (BinTree a)
      deriving (Eq, Ord, Show, Read)
 
 -- als waarde hoger dan boomwaarde, ga naar rechts anders links
--- push :: (Ord a) => BinTree a -> a -> BinTree a
--- push Empty x = Node Empty x Empty
--- push (Node a y b) x = Node a (Node Empty x Empty) b
 
+push Empty x = Node Empty x Empty
+push (Node a y b) x
+    | x < y = Node (push a x) y b
+    | otherwise = Node a y (push b x)
+    
 mapTree :: (t -> a) -> BinTree t -> BinTree a
 mapTree _ Empty =  Empty
 mapTree f (Node leftTree a rightTree) = Node (mapTree f leftTree) (f a) (mapTree f rightTree)

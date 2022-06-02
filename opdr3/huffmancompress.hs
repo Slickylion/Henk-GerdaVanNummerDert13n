eec3560acd5c482b4a1cd29a71bf20cdd1fa94fa
Slickylion <Slickylion@gmail.com>
2022-06-02 15:58:50 +0200

    Added countLetter

--- opdr3/huffmancompress.hs
+++ opdr3/huffmancompress.hs
@@ -3,3 +3,15 @@ module Main where
 main :: IO ()
 main = do
     putStrLn "HEUFMAN"
+
+
+countLetters :: String -> [String]
+countLetters [] = []
+countLetters (x:rest) = ["hou je mond compiler"]
+
+countLetter :: Char -> String -> Int 
+countLetter letter [] = 0
+countLetter letter (x:rest)
+    | letter == x = 1 + recursive
+    | otherwise = recursive
+    where recursive = countLetter letter rest

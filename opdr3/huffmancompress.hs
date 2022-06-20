8457592b02f7e377761bcd675006d430de9d79b2
Slickylion <Slickylion@gmail.com>
2022-06-20 14:12:56 +0200

    Sort map into list

--- opdr3/huffmancompress.hs
+++ opdr3/huffmancompress.hs
@@ -3,14 +3,25 @@ module Main where
 import Data.Map as M
 import Prelude as P
 import Data.Maybe
+import System.Environment
+import Data.Function
+import Data.List
 
 type Mappy = M.Map Char Int
 
 main :: IO ()
 main = do
-    let maap = M.insert 'a' 9
-    let x = M.lookup 'a'
-    putStrLn "HEUFMAN"
+    args <- getArgs
+    case args of
+        [arg1, arg2, arg3] -> do
+            text <- readFile arg1;
+            let text2 = sortBy (flip compare `on` snd) (toList (countLetters text empty))
+            print text2
+            putStrLn "YES WE GOT EM"
+        _ -> putStrLn "NOT ENOUGH ARGUMENTS, give file to compress, file to save compressed, and file to save tree"
+
+    -- De file waar de tree naartoe wordt geschreven
+
 
 
 countLetters :: String -> Mappy-> Mappy
